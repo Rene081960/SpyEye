61ef0e111402355545601cd0e56bfa2f9d5b6a9c
René van Dorp <dorpr@wigo4it.nl>
2020-09-04 16:30:25 +0200

    Solution toegevoegd

---
+++ eyes/eyes/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Arduino_Eyes = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ShowToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 50
+        '
+        'Arduino_Eyes
+        '
+        Me.Arduino_Eyes.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.Arduino_Eyes.Icon = CType(resources.GetObject("Arduino_Eyes.Icon"), System.Drawing.Icon)
+        Me.Arduino_Eyes.Text = "Arduino Eyes"
+        Me.Arduino_Eyes.Visible = True
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem, Me.ShowToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(100, 48)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'ShowToolStripMenuItem
+        '
+        Me.ShowToolStripMenuItem.Name = "ShowToolStripMenuItem"
+        Me.ShowToolStripMenuItem.Size = New System.Drawing.Size(99, 22)
+        Me.ShowToolStripMenuItem.Text = "Hide"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Connected to"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 50)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Label2"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(216, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(115, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Reset Connection"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.Black
+        Me.TextBox1.Font = New System.Drawing.Font("Consolas", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.TextBox1.Location = New System.Drawing.Point(15, 78)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(316, 201)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 291)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Arduino Spying Eyes"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Arduino_Eyes As NotifyIcon
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShowToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ eyes/eyes/Form1.vb
@@ -0,0 +1,153 @@
+﻿Imports System.Threading
+Imports System.IO.Ports
+
+Public Class Form1
+
+    Dim lastx, lasty As Int16
+    Dim closeme As Boolean = False
+
+    Dim checkMouse As Boolean = True
+
+
+    Dim ArduinoPort As SerialPort = Nothing
+
+
+    Private Sub ShowToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShowToolStripMenuItem.Click
+        If ShowToolStripMenuItem.Text = "Show" Then
+            Me.Show()
+            Me.WindowState = FormWindowState.Normal
+            ShowToolStripMenuItem.Text = "Hide"
+        Else
+            ShowToolStripMenuItem.Text = "Show"
+
+            Me.WindowState = FormWindowState.Minimized
+            Me.Hide()
+        End If
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        closeme = True
+        Me.Close()
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ShowToolStripMenuItem.Text = "Show"
+        Me.WindowState = FormWindowState.Minimized
+        Me.Hide()
+        If Not closeme Then e.Cancel = True
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim x, y, w, h As Int16
+        Dim strsend = ""
+        w = Screen.PrimaryScreen.Bounds.Width
+        h = Screen.PrimaryScreen.Bounds.Height
+        x = Cursor.Position.X
+        y = Cursor.Position.Y
+        Label2.Text = "mouse at:" & x.ToString & "," & y.ToString
+
+        x = Math.Round(10 * (x / w))
+        y = Math.Round(10 * (y / h))
+        If (x > 9) Then x = 9
+        If (y > 9) Then y = 9
+
+        If Not ArduinoPort.IsOpen Then
+            Label1.Text = "Connection lost!"
+            TextBox1.AppendText(vbNewLine & "ERROR: Connection lost!" & vbNewLine)
+            Timer1.Enabled = False
+            Exit Sub
+        End If
+
+        If (lastx <> x Or lasty <> y) Then
+            strsend = "xy" + x.ToString + y.ToString
+            lastx = x
+            lasty = y
+            ArduinoPort.WriteLine(strsend)
+        End If
+
+        If MouseButtons = MouseButtons.None Then checkMouse = True
+
+        If checkMouse And MouseButtons = MouseButtons.Left Then
+            strsend = "by" + x.ToString + y.ToString
+            checkMouse = False
+            ArduinoPort.WriteLine(strsend)
+        End If
+
+
+    End Sub
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click, MyBase.Load
+        Label1.Text = "Not connected"
+        If ArduinoPort IsNot Nothing Then
+            If ArduinoPort.IsOpen Then ArduinoPort.Close()
+        End If
+        TextBox1.AppendText(vbNewLine)
+        If Not FindArduino().Equals("") Then
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+
+
+
+
+    Private Function FindArduino() As String
+        Timer1.Enabled = False
+        TextBox1.AppendText("Searching for Arduino." & vbNewLine)
+        ' see https://docs.microsoft.com/en-us/dotnet/visual-basic/developing-apps/programming/computer-resources/how-to-receive-strings-from-serial-ports
+        Dim AvailablePorts() As String = SerialPort.GetPortNames()
+        Dim port As String = ""
+
+        If My.Computer.Ports.SerialPortNames.Count < 1 Then
+            TextBox1.AppendText("No serial ports available." & vbNewLine)
+            Return ""
+        End If
+        For Each port In AvailablePorts
+            ' try to connect to serial port sp
+
+            Try
+                ArduinoPort = New SerialPort()
+                TextBox1.AppendText("Checking port:" & port & vbNewLine)
+                ArduinoPort.DtrEnable = False 'this should prevent the arduino to reset on connecting!
+                ArduinoPort.PortName = port
+                ArduinoPort.ReadTimeout = 500
+                ArduinoPort.BaudRate = 57600
+                ArduinoPort.Parity = Parity.None
+                ArduinoPort.DataBits = 8
+                ArduinoPort.StopBits = StopBits.One
+
+                Try
+                    ArduinoPort.Open()
+                Catch
+                    Continue For
+                End Try
+
+
+                ' the arduino will reset when we connect, so give it some time to boot
+                Thread.Sleep(2500)
+
+                ' write V to serial port
+                ArduinoPort.WriteLine("V")
+                ' read the response
+                Dim response As String = ArduinoPort.ReadLine()
+                If response.Contains("Arduino-eyes") Then
+                    ' we found the arduino project!
+                    Label1.Text = "Connected to " & port
+                    TextBox1.AppendText("Found Arduino on port:" & port & vbNewLine)
+                    'ArduinoPort.Close()
+                    Return port
+                Else
+                    ArduinoPort.Close()
+                End If
+
+            Catch ex As TimeoutException
+                'TextBox1.AppendText("Error: Serial Port read timed out." & vbNewLine)
+            End Try
+
+        Next
+        Return port
+    End Function
+End Class
